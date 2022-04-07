e3c2809aa0aa193afa47794ab0595c861b3d9f43
Balki <balki14101@gmail.com>
2022-04-08 00:39:46 +0530

    Chore: Final Commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,96 +1,202 @@
 // library imports
 import React, {useEffect} from 'react';
-import {View, Text, Dimensions, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
 import {connect} from 'react-redux';
 
 // actions
 import {fetchUserDetails} from '../../redux/actions/user';
-const Width = Dimensions.get('screen').width;
-const Height = Dimensions.get('screen').height;
 
 /**
- * @function Counter
- * @description bla kuasdjajdjhasnkjasd
+ * @function Home
+ * @description Main screen
  * @returns {JSX}
  */
 const Home = props => {
+  // called for the first time when screen is mounted - componentDidMount
   useEffect(() => {
-    triggerFetchUser();
+    // purposefully making to wait for 3 seconds to show loader
+    setTimeout(() => {
+      triggerFetchUser();
+    }, 3000);
   }, []);
+
   const {loading, triggerFetchUser, userDetails} = props;
   const renderRow = (data = {}) => {
     return (
       <View
         key={String(data?.index)}
-        style={{
-          marginHorizontal: 4,
-          flexDirection: 'row',
-          padding: 4,
-        }}>
+        style={[
+          {
+            backgroundColor: isNaN(data?.index) ? '#add8e6' : 'transparent',
+          },
+          styles.rowContainer,
+        ]}>
         <View
-          style={{
-            backgroundColor: 'green',
-            width: Width / 4,
-            padding: 4,
-          }}>
-          <Text>{data?.first || '-'}</Text>
+          style={[
+            {
+              borderBottomColor:
+                userDetails?.length === data?.index + 1
+                  ? 'gray'
+                  : 'transparent',
+            },
+            styles.firstFourColumns,
+          ]}>
+          <Text
+            style={[
+              styles.cellText,
+              {
+                fontWeight: isNaN(data?.index) ? 'bold' : 'normal',
+              },
+            ]}>
+            {data?.first || '-'}
+          </Text>
         </View>
         <View
-          style={{
-            backgroundColor: 'lightblue',
-            width: Width / 2,
-          }}>
-          <Text>{data?.second || '-'}</Text>
+          style={[
+            {
+              borderBottomColor:
+                userDetails?.length === data?.index + 1
+                  ? 'gray'
+                  : 'transparent',
+            },
+            styles.firstFourColumns,
+          ]}>
+          <Text
+            style={[
+              styles.cellText,
+              {
+                fontWeight: isNaN(data?.index) ? 'bold' : 'normal',
+              },
+            ]}>
+            {data?.second || '-'}
+          </Text>
         </View>
         <View
-          style={{
-            backgroundColor: 'yellow',
-            width: Width / 2,
-          }}>
-          <Text>{data?.third || '-'}</Text>
+          style={[
+            {
+              borderBottomColor:
+                userDetails?.length === data?.index + 1
+                  ? 'gray'
+                  : 'transparent',
+            },
+            styles.firstFourColumns,
+          ]}>
+          <Text
+            style={[
+              styles.cellText,
+              {
+                fontWeight: isNaN(data?.index) ? 'bold' : 'normal',
+              },
+            ]}>
+            {data?.third || '-'}
+          </Text>
         </View>
-        <View style={{backgroundColor: 'pink', width: Width / 2}}>
-          <Text>{data?.fourth || '-'}</Text>
+        <View
+          style={[
+            {
+              borderBottomColor:
+                userDetails?.length === data?.index + 1
+                  ? 'gray'
+                  : 'transparent',
+            },
+            styles.firstFourColumns,
+          ]}>
+          <Text
+            style={[
+              styles.cellText,
+              {
+                fontWeight: isNaN(data?.index) ? 'bold' : 'normal',
+              },
+            ]}>
+            {data?.fourth || '-'}
+          </Text>
         </View>
-        <View style={{backgroundColor: 'orange', width: Width / 2}}>
-          <Text>{data?.fifth || '-'}</Text>
+        <View
+          style={[
+            {
+              borderBottomColor:
+                userDetails?.length === data?.index + 1
+                  ? 'gray'
+                  : 'transparent',
+            },
+            styles.firstFourColumns,
+            {borderRightColor: 'gray'},
+          ]}>
+          <Text
+            style={[
+              styles.cellText,
+              {
+                fontWeight: isNaN(data?.index) ? 'bold' : 'normal',
+              },
+            ]}>
+            {data?.fifth || '-'}
+          </Text>
         </View>
       </View>
     );
   };
   return userDetails ? (
-    <ScrollView horizontal>
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: 'center',
-        }}>
-        {renderRow({
-          first: 'Username',
-          second: 'Email',
-          third: 'City',
-          fourth: 'Phone',
-          fifth: 'Company Name',
-        })}
-        {userDetails?.map((i, index) => {
-          return renderRow({
-            first: i?.name,
-            second: i?.email,
-            third: i?.address?.city,
-            fourth: i?.phone,
-            fifth: i?.company?.name,
-            index: index,
-          });
-        })}
-      </ScrollView>
+    <ScrollView
+      contentContainerStyle={{
+        alignItems: 'center',
+        paddingVertical: 8,
+      }}>
+      {/* To show the header of the table */}
+      {renderRow({
+        first: 'Username',
+        second: 'Email',
+        third: 'City',
+        fourth: 'Phone',
+        fifth: 'Company Name',
+      })}
+      {/* Render the contents of the table here */}
+      {userDetails?.map((i, index) => {
+        return renderRow({
+          first: i?.name,
+          second: i?.email,
+          third: i?.address?.city,
+          fourth: i?.phone,
+          fifth: i?.company?.name,
+          index: index,
+        });
+      })}
     </ScrollView>
   ) : (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{marginVertical: 16, color: 'black'}}>
-        {'Data not fetched yet'}
-      </Text>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text style={{marginVertical: 16, color: 'black'}}>
+          {'Data not fetched yet'}
+        </Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cellText: {
+    color: 'black',
+    fontSize: 10,
+  },
+  firstFourColumns: {
+    borderWidth: 1,
+    borderLeftColor: 'gray',
+    borderTopColor: 'gray',
+    borderRightColor: 'transparent',
+    flex: 1,
+    padding: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rowContainer: {marginHorizontal: 8, flexDirection: 'row'},
+});
 
 const mapStateToProps = (state, props) => {
   return {
